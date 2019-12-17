fbbe0c4fc363ba0e605d48cfb242294dc8d4ae84
ZYXWZ <m18256636979@163.com>
2019-12-17 17:39:16 +0800

    xiaobao1217

---
+++ xb/js/text.js
@@ -0,0 +1,13 @@
+var msg =[
+    {xiaoming:{alt:'图片', name:'xiaoming',age:18,sex:'man',height:170,weight:50}},
+    {name:'xiaohong',age:18,sex:'woman',height:160,weight:40},
+    {name:'xiaohong',age:18,sex:'woman',height:160,weight:40},
+    {name:'xiaohong',age:18,sex:'woman',height:160,weight:40},
+    {name:'xiaohong',age:18,sex:'woman',height:160,weight:40},
+    {name:'xiaohong',age:18,sex:'woman',height:160,weight:40},
+    {name:'xiaohong',age:18,sex:'woman',height:160,weight:40},
+];
+var mess = JSON.stringify(msg);
+function getData(){
+    return mess;
+}
