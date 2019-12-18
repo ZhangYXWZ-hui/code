97b737173b54ee0ba691495758967ccc046820ee
ZYXWZ <m18256636979@163.com>
2019-12-18 17:36:06 +0800

    xiaobao1218

---
+++ xb/js/data.js
@@ -0,0 +1,36 @@
+let msg = 
+    {xiaobao:{
+        index:[],
+        dianpu:{
+            program:{
+                title:'UCC干洗',
+                money:'5~10万',
+                time:'2013-06-07',
+                instudry:'干洗',
+                count:'10000家',
+                area:'全国',
+                people:'自由创业',
+                scope:'干洗店加盟',
+                auth:'是',
+                birthplace:'上海',
+                contract:'',
+                train:'',
+                royalties:'',
+                condition:'',
+                margin:'',
+                initial:''
+            },
+            company:{
+                name:'上海云端洗烫设备集团有限公司',
+                nature:'有限责任公司',
+                funds:'1000万元',
+                location:'上海市宝山区沪太'
+            }
+        },
+        dianpuindex:[]
+    }}
+;
+let mess = JSON.stringify(msg);
+function getData(){
+    return mess;
+}
